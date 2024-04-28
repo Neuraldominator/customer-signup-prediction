e5c1277cf22f581d8fd36fddc3fa8c96e8c81b75
Dominik Kessler <D39220@eon.com>
2024-04-28 22:59:54 +0200

    Get install_requires from requirements.txt

--- setup.py
+++ setup.py
@@ -2,6 +2,9 @@ from setuptools import find_packages, setup
 
 with open("README.md", "r") as f:
     long_description = f.read()
+    
+with open('requirements.txt') as f:
+    requirements = f.readlines()
 
 setup(
     name="customer-signup-prediction",
@@ -21,23 +24,7 @@ setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    install_requires=["chardet==5.2.0",
-                     # "charset-normalizer==3.3.2",
-                      "ftfy==6.2.0",
-                      "fuzzywuzzy==0.18.0",
-                     # "Levenshtein==0.25.1"
-                      "ipykernel==6.29.4",
-                      "ipython==8.12.3",
-                      "matplotlib==3.7.5",
-                      "numpy==1.24.4",
-                      "pandas==2.0.3",
-                      "seaborn==0.13.2",
-                      #jupyter_client==8.6.1
-                      #jupyter_core==5.7.2
-                      #jupyterlab_pygments==0.3.0
-                      #matplotlib-inline==0.1.7
-                      #rapidfuzz==3.8.1
-    ],
+    install_requires=requirements,
     extras_require={},
     python_requires="==3.8",
 )
